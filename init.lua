3e05ff7b96525e0be31c1505efd2103119a3eb03
lubiksss <lubiksss@gmail.com>
2023-11-21 12:02:20 +0900

    feat: Remove vim setting

--- init.lua
+++ init.lua
@@ -3,6 +3,3 @@ local FRemap = require('foundation_remapping')
 local remapper = FRemap.new()
 remapper:remap('rcmd','f18')
 remapper:register()
-
-require('./modules/change_input_source_if_ko'):bind_change_input_source_with({}, "escape")
-require('./modules/vim_keymap'):bind_vim_keymap_with({"cmd","shift"}, "space")
